9f8f2d870000fa030aee8a8e8b2a30eaf3929d76
akivarichman <121504299+akivarichman@users.noreply.github.com>
2023-09-26 21:05:44 -0400

    lab complete

--- src/App.js
+++ src/App.js
@@ -16,6 +16,15 @@ function App() {
 
   let card;
 
+  const handleClick = (event) => {
+    fetch("https://opentdb.com/api.php?amount=1&category=9&type=multiple")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuestionData(data.results[0])
+        setSelectedAnswer(null)
+      })
+  }
+
   if (selectedAnswer) {
     card = (
       <ResultCard
@@ -41,7 +50,7 @@ function App() {
     <div className="w-100 my-5 d-flex justify-content-center align-items-center">
       <div style={{ maxWidth: "45%" }}>
         <h1 className="text-center">Trivia App</h1>
-        <button className="btn btn-success">Next Question</button>
+        <button className="btn btn-success" onClick={handleClick}>Next Question</button>
         {card}
       </div>
     </div>
